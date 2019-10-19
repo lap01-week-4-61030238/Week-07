d21cee2125ac0f8aa770da14dcd0591c08973d0c
“Saya” <61030238@kmitl.ac.th>
2019-10-19 11:38:10 +0700

    5

--- 61030238/ConsoleApp2/ConsoleApp2/Program.cs
+++ 61030238/ConsoleApp2/ConsoleApp2/Program.cs
@@ -12,21 +12,56 @@ namespace ConsoleApp1
         {
 
 
-            Student su = new Student();
-            su.Name = "Student Name";
-            su.ID = "12345678";
-            su.GPA = 7.6f;
-            Console.WriteLine("Student name : " + su.Name);
-            Console.WriteLine("Student ID   : " + su.ID);
-            Console.WriteLine("Student GPA  : " + su.GPA);
+            Student su = new Student("Student Name", "12345678");
+            try
+            {
+                
+                su.GPA = 3.5f;
+                Console.WriteLine("Student name : " + su.Name);
+                Console.WriteLine("Student ID   : " + su.ID);
+                Console.WriteLine("Student GPA  : " + su.GPA);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
     class Student
     {
-        private string Name;
-        private string ID;
-        private float GPA;
+        private string name;
+        private string id;
+        private float gpa;
+
+        public Student(string Name, string ID)
+        {
+            this.name = Name;   // assign auto variable to the field
+            this.id = ID;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+        public string ID
+        {
+            get { return id; }
+        }
+        public float GPA
+        {
+            get
+            {
+                return gpa;
+            }
+            set
+            {
+                if (value > 0.0 && value <= 4.0)
+                    gpa = value;
+                else
+                    throw (new Exception("Error!!!! invalid GPA"));
+            }
+        }
     }
 }
 
