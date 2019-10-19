84ae6678328f7f66f0f6b8faab3ab06ac8164df7
“Saya” <61030238@kmitl.ac.th>
2019-10-20 05:26:38 +0700

    10

--- 61030238/ConsoleApp2/ConsoleApp2/Program.cs
+++ 61030238/ConsoleApp2/ConsoleApp2/Program.cs
@@ -11,33 +11,43 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            int StudentID;
-            StudentID = int.Parse(Console.ReadLine());
-
-
-
+            Vector v1 = new Vector(1, 4);
+            Vector v2 = new Vector(8, 5);
+            Vector v3 = v1 + v2;
+            Console.WriteLine("vector v1 = " + v1);
+            Console.WriteLine("vector v2 = " + v2);
+            Console.WriteLine("vector v3 = " + v3);
+            Console.WriteLine("-v3 = " + (-v3));
+            Console.ReadKey();
         }
-        class TraineeStudent
+    }
+    class Vector
+    {
+        public Vector(int x, int y)
         {
-            private string StudentID;
-            private string salary;
-            public TraineeStudent(string ID, string SALARY)
-            {
-                this.StudentID = ID;
-                this.salary = SALARY;
-            }
-            static void getSalary(add int StudentID)
-            {
-                return StudentID;
-            }
-            static void getStudentID()
-            {
-                Console.WriteLine(salary);
-            }
+            this.X = x;
+            this.Y = y;
+        }
+        public int X { get; set; }  // X - accessor (property)
+        public int Y { get; set; }  // Y - accessor (property)
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", X, Y);
+        }
+        // All operator overloads must be public and static.
+        public static Vector operator +(Vector v1, Vector v2) //binary operator +
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+        public static Vector operator -(Vector v1)  //unary operator -
+        {
+            return new Vector(-v1.X, -v1.Y);
+        }
 
         }
 
     }
-}
+
 
 
